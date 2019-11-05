d323d2f5c093d85342338c7c7c6d5662fc1e2bab
Fernando Ventilari <fernandoventilari@gmail.com>
2019-11-05 00:15:44 -0400

    Calculadora de IMC terminada - V1

---
+++ script/script.js
@@ -0,0 +1,56 @@
+
+var deny=["-", "+", "e"];
+
+function calculate(){
+
+	var weight=Number(winput.value), height=Number(hinput.value)/100, result;
+
+	if(weight>0 && height>0){
+		result=weight/(height*height);
+		result=Math.round(result*100)/100;
+
+		number.innerHTML=result;
+		if(result<18.5) imc="Abaixo do peso";
+		else if(result<25) imc="Normal";
+		else if(result<30) imc="Sobrepeso";
+		else if(result<40) imc="Obesidade";
+		else imc="Obesidade grave";
+
+		right.style.opacity=1.0;
+	}
+	else{
+		right.style.opacity=0.2;
+		number.innerHTML="--.-";
+		imc="IMC";
+	}
+
+}
+
+function logKey(event){
+
+	var x=event.target;
+	var i;
+	for(i=0;i<deny.length;i++){
+		if(event.key==deny[i]){
+			event.preventDefault();
+			return;
+		}
+	}
+
+}
+
+function start(){
+
+	winput=document.getElementById("weight"), hinput=document.getElementById("height"), right=document.getElementById("right"),
+	number=document.getElementById("number"), imc=document.getElementById("result");
+
+	winput.addEventListener("keydown", logKey);
+	hinput.addEventListener("keydown", logKey);
+
+	winput.addEventListener("keyup", calculate);
+	hinput.addEventListener("keyup", calculate);
+
+}
+
+
+
